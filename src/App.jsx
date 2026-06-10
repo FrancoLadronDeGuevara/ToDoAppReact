13f0d8d150b201e36cf5042409227becf1312fe1
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2026-06-10 20:04:44 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { FaTasks } from "react-icons/fa";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold flex items-center justify-center gap-2 mt-5">
+        <FaTasks className="text-green-600" />
+        <span className="text-blue-950">Mi Lista de Tareas</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
